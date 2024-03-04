0ebb4a683c63d14f4ffb73de7ffef7ac06be52b0
Lukas Polak <admin@lukaspolak.cz>
2024-03-04 18:35:41 +0100

    Add first generic prototype

--- frontend/client/visualize/app.tsx
+++ frontend/client/visualize/app.tsx
@@ -2,13 +2,29 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 
 import "./app.css";
+import { FirstType, FirstTypeProps } from "./firstType";
 
-export function render() {
+type Visualization<T> = {
+    type: string;
+    data: T;
+};
+
+export function render<T>(visualization: Visualization<T>) {
     const container = document.getElementById("content");
     const pocketListRoot = createRoot(container!);
-    pocketListRoot.render(<MainVis />);
+    pocketListRoot.render(<MainVis type={visualization.type} data={visualization.data} />);
 }
 
-export function MainVis() {
-    return (<div>Hi</div>);
+function MainVis<T>({ type, data }: Visualization<T>) {
+    switch (type) {
+        case "firstType":
+            return <FirstType {...(data as FirstTypeProps)} />;
+        default:
+            return (
+                <div>
+                    <h3>Unknown visualization type</h3>
+                    <pre>{JSON.stringify({ type, data })}</pre>
+                </div>
+            );
+    }
 }
--- frontend/client/visualize/visualize.ts
+++ frontend/client/visualize/visualize.ts
@@ -2,4 +2,5 @@ import "../bootstrap.scss";
 import "bootstrap";
 import { render } from "./app";
 
-render();
+render({ type: "firstType", data: { message: "hi" } });
+// render({ type: "secondType", data: { message: "hi2" } });
