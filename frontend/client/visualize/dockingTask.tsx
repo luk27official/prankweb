cbabb1d143f1b2e2f68ea2211eb3874632d47e6d
Lukas Polak <admin@lukaspolak.cz>
2024-03-28 14:28:07 +0100

    Add ligand to Mol*, add button to change models

--- frontend/client/visualize/dockingTask.tsx
+++ frontend/client/visualize/dockingTask.tsx
@@ -8,6 +8,8 @@ import 'molstar/lib/mol-plugin-ui/skin/light.scss';
 import { loadStructureIntoMolstar } from "../viewer/molstar-visualise";
 import { PluginUIContext } from "molstar/lib/mol-plugin-ui/context";
 import { getApiEndpoint } from "../prankweb-api";
+import { PluginCommands } from "molstar/lib/mol-plugin/commands";
+import { UpdateTrajectory } from "molstar/lib/mol-plugin-state/actions/structure";
 
 export interface DockingTaskProps {
     content: string;
@@ -17,26 +19,75 @@ export interface DockingTaskProps {
     structureName: string;
 }
 
+let dockedMolecule: any; // to be able to access the docked molecule from here and avoid multiple fetches
+
 export function DockingType(dp: DockingTaskProps) {
+    const [plugin, setPlugin] = React.useState<PluginUIContext | undefined>(undefined);
+    const [model, setModel] = React.useState<number>(1);
+
     // this is a hook that runs when the component is mounted
     useEffect(() => {
         const loadPlugin = async () => {
             const plugin: PluginUIContext = await createMolstarViewer();
+            setPlugin(plugin);
 
             const baseUrl: string = getApiEndpoint(dp.database, dp.id) + "/public";
             // Download pdb/mmcif and create a model in Mol*.
             const molData = await loadStructureIntoMolstar(plugin, `${baseUrl}/${dp.structureName}`).then(result => result);
+            // Load the docked ligand into Mol*.
+            const ligandData = await loadLigandIntoMolstar(plugin, dockedMolecule);
         };
         loadPlugin();
     }, []);
+
+    useEffect(() => {
+        // TODO: fix the "model" counter - not needed now, but the number should correspond
+        const updateModel = async () => {
+            if (plugin === undefined) {
+                return;
+            }
+
+            PluginCommands.State.ApplyAction(plugin, {
+                state: plugin.state.data,
+                action: UpdateTrajectory.create({ action: 'advance', by: 1 })
+            });
+        };
+
+        updateModel();
+    }, [model]);
 
     return <div style={{ display: "flex" }}>
         <div style={{ width: "50%", margin: "5px" }}>
             <h3>Docking Task</h3>
             <div id="molstar-wrapper" style={{ width: "100%", position: "relative", height: "50vh" }}></div>
         </div>
-        <div id="content-wrapper" style={{ width: "50%", margin: "5px" }}><pre>{dp.content}</pre></div>
+        <div id="content-wrapper" style={{ width: "50%", margin: "5px" }}>
+            <div><button onClick={() => setModel(model + 1)}>Change model</button></div>
+            <div><pre>{dp.content}</pre></div>
+        </div>
     </div>;
+}
+
+async function loadLigandIntoMolstar(plugin: PluginUIContext | undefined, dockedMolecule: any) {
+    if (dockedMolecule === undefined || plugin === undefined) {
+        return;
+    }
+
+    const ligandData = await plugin.builders.data.rawData({ data: dockedMolecule });
+    const trajectory = await plugin.builders.structure.parseTrajectory(ligandData, "pdbqt");
+    const model = await plugin.builders.structure.createModel(trajectory);
+    const structure = await plugin.builders.structure.createStructure(model, { name: 'model', params: {} });
+
+    let repr;
+    // TODO: Think about more representations.
+    const ligand = await plugin.builders.structure.tryCreateComponentStatic(structure, "ligand");
+    if (ligand) {
+        repr = await plugin.builders.structure.representation.addRepresentation(ligand, {
+            type: 'ball-and-stick',
+        });
+    }
+
+    return [model, structure, repr];
 }
 
 async function createMolstarViewer() {
@@ -85,6 +136,8 @@ export async function getDockingTaskContent(type: string, id: string, database: 
     if (response === undefined) {
         return { content: "Error", hash: hash, id: id, database: database, structureName: structureName };
     }
+
+    dockedMolecule = response;
 
     return { content: response, hash: hash, id: id, database: database, structureName: structureName };
 }
