1eac2b8d2218258371a42d5c1889d125e23c1e45
Lukas Polak <admin@lukaspolak.cz>
2024-09-03 22:38:33 +0200

    Remove unused CSS file

--- frontend/client/visualize/app.tsx
+++ frontend/client/visualize/app.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-import "./app.css";
 import { DockingTask } from "./docking/main";
 import { DockingTaskProps } from "./docking/types";
 
